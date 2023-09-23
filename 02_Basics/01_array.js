7bac320e91e8ab52a568cf1e0322302867c5c081
0333Aniket <145827200+0333Aniket@users.noreply.github.com>
2023-09-23 14:45:40 +0000

    Arry

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,27 @@
+// //Array
+// const myArray = [0,1,2,3,4,5]
+// const Array2 =["Shaktiman","nagraj","Spiderman"]
+const myArray2 =new Array(1,2,3,4,5)
+console.log(myArray2[2])
+
+// Array Methods
+ //myArray2.push(6)   // Push Methode ADD New Element In By Defoult Last Index   EX -->[ 1, 2, 3, 4, 5, 6 ]
+//  console.log(myArray2);
+//  myArray2.push(7,6,8)
+// console.log(myArray2)
+// myArray2.pop()  // POP Methide use in remove last element by defoult last index  
+console.log(myArray2);
+myArray2.unshift(9,8 )  //Unshift Methode add new Element in Array  and give a new lenght by default add Element in start mins 0 index
+console.log(myArray2)
+myArray2.shift(9,8)
+console.log(myArray2)
+myArray2.shift()
+console.log(myArray2)
+
+//  ++++++++++++++++++++++++++++++++++++++++++ JOIN +++++++++++++++++++++++++++++++++++++++++++++
+
+const newArr = myArray2.join()  // in Join Method or Function To Convart arry to string
+console.log(newArr)
+console.log(typeof newArr)      // atta newArry ha array element ahe pn to string ahe
+
+ //++++++++++++++++++++++++++++++++ Slice or Splice ++++++++++++++++++++++++++++++++++
