b6d63ebdd8da7ac50539840c2d7b1226c10601be
0333Aniket <145827200+0333Aniket@users.noreply.github.com>
2023-09-23 10:41:54 +0000

    Datatyape

--- 01_Basic.js
+++ 01_Basic.js
@@ -1,1 +1,25 @@
-console.log("Aniket");
+// console.log("Aniket");
+// let Myname="Hitesh Ka channnel chai or code"
+// console.log(Myname[0]);
+// console.log(Myname)
+// const repo=23
+// console.log(Myname+" "+repo)
+// console.log(Myname.length)
+// console.log(`im Aniket & i wil Learn a java to ${Myname} in ${repo} day`)
+const gameName = new String('Aniket')
+// console.log(gameName);
+// console.log(gameName.length)
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('k'));
+
+// const newStirng =gameName.substring(1,3)
+// console.log(newStirng);
+// let ab =newStirng
+// console.log(ab)
+// console.log(newStirng)
+const anotherString =gameName.slice(-6,2)
+console.log(anotherString);
+console.log(gameName.length);
+const newStirng2="   Hi H-i-t-e-s-h      "
+console.log(newStirng2.trim())
+console.log(newStirng2.split(' '))
